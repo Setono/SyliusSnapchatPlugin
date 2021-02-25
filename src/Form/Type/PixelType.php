7730f50a9c2945c64290706b463acadb463a72f7
Joachim Løvgaard <joachim@loevgaard.dk>
2021-02-25 08:20:34 +0100

    Fix form type to allow strings instead of integers

--- src/Form/Type/PixelType.php
+++ src/Form/Type/PixelType.php
@@ -7,7 +7,7 @@ namespace Setono\SyliusSnapchatPlugin\Form\Type;
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class PixelType extends AbstractResourceType
@@ -15,7 +15,7 @@ final class PixelType extends AbstractResourceType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pixelId', IntegerType::class, [
+            ->add('pixelId', TextType::class, [
                 'label' => 'setono_sylius_snapchat.form.pixel.pixel_id',
                 'attr' => [
                     'min' => 1,
